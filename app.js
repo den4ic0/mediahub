938e1cea08015e2db4ff84d648ead9f40ab8528d
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-05-16 21:45:46 +0300

    add optimization

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ dotenv.config();
 
 const app = express();
 
+const fileExistenceCache = {};
+
+const uploadPath = process.env.UPLOAD_PATH || 'uploads';
+const port = process.env.PORT || 3000;
+
 function logActivity(activity) {
   console.log(`${new Date().toISOString()} - ${activity}`);
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const dest = process.env.UPLOAD_PATH || 'uploads/';
-    logActivity(`Destination set for file upload: ${dest}`);
-    cb(null, dest);
+    logActivity(`Destination set for file upload: ${uploadPath}`);
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const filename = `${Date.now()}-${file.originalname}`;
@@ -30,11 +34,12 @@ const upload = multer({
   limits: { fileSize: 50 * 1024 * 1024 },
 });
 
-app.use('/media', express.static(process.env.UPLOAD_PATH || 'uploads'));
+app.use('/media', express.static(uploadPath));
 
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
     logActivity(`File uploaded: ${req.file.filename}`);
+    fileExistenceCache[req.file.filename] = true;
     return res.json({
       message: 'File uploaded successfully',
       fileInfo: req.file
@@ -47,15 +52,32 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
 app.get('/file-info', (req, res) => {
   const fileName = req.query.filename;
-  const filePath = path.join(process.env.UPLOAD_PATH || 'uploads', fileName);
+  const filePath = path.join(uploadPath, fileName);
+
+  if (fileExistenceCache[fileName] !== undefined) {
+    if (fileExistenceCache[fileName]) {
+      logActivity(`File info accessed from cache: ${fileName}`);
+      return res.json({
+        message: 'File exists',
+        filename: fileName,
+        url: `${req.protocol}://${req.get('host')}/media/${fileName}`
+      });
+    } else {
+      logActivity(`File not found (cached): ${fileName}`);
+      return res.status(404).send('File not found');
+    }
+  }
 
   if (fs.existsSync(filePath)) {
     logActivity(`File info accessed: ${fileName}`);
+    fileExistenceCache[fileName] = true;
     return res.json({
       message: 'File exists',
       filename: fileName,
       url: `${req.protocol}://${req.get('host')}/media/${fileName}`
     });
+  } else {
+    fileExistenceCache[fileName] = false;
   }
 
   logActivity(`File not found: ${fileName}`);
@@ -67,7 +89,6 @@ app.use((req, res) => {
   res.status(404).send('Route not found');
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   logActivity(`Server started on port ${port}`);
 });
